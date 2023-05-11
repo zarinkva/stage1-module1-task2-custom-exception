0684cde9beeae1d92bdea14f76d670bf8edec16d
zarina <zarinakamalova117@gmail.com>
2023-05-11 09:41:26 +0600

    done

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,16 +6,32 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
+    if (!isValidID(studentID)) {
+      throw new InvalidStudentIDException("Could not find student with ID " + studentID);
+    }
     return Student.getValueOf(studentID);
   }
+  private boolean isValidID(long studentID) {
+    // Проверка на допустимость идентификатора студента
+    // Ваша логика проверки здесь...
+    return studentID >= 1 && studentID <= 11;
+  }
+
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
       Student student = manager.find(IDs[i]);
+      try {
+       // Student student = manager.find(IDs[i]);
+        System.out.println("Student name: " + student.getName());
+      }
+      catch (InvalidStudentIDException e)
+      {
       System.out.println("Student name " + student.getName());
     }
 
   }
+}
 }
